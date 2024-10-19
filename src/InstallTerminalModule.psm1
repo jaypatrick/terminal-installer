b2cbe723fafc7d8c70417ebda1dd65fd8a0b89bd
Jayson Knight <jayson.knight@jaysonknight.com>
2024-10-18 22:29:35 -0400

    Restructured directories, added pester test

---
+++ src/InstallTerminalModule.psm1
@@ -0,0 +1,33 @@
+function Install-WindowsTerminal {
+    param (
+        [string]$repo = "microsoft/terminal",
+        [string]$workingDirectory
+    )
+
+    # Get the latest terminal version
+    $terminalVersionString = Get-LatestReleaseVersion -repo $repo
+
+    # Define the bin directory path
+    $binDirectory = Join-Path -Path $workingDirectory -ChildPath ".\bin"
+
+    # Ensure the bin directory exists
+    if (-not (Test-Path -Path $binDirectory)) {
+        New-Item -ItemType Directory -Path $binDirectory -Force
+    }
+
+    # Define terminal package strings
+    $terminalPackageUrl = "https://github.com/$repo/releases/download/v$($terminalVersionString)/Microsoft.WindowsTerminal_$($terminalVersionString)_8wekyb3d8bbwe.msixbundle"
+    $terminalPackageOutName = "Microsoft.WindowsTerminal_$($terminalVersionString)_8wekyb3d8bbwe.msixbundle"
+
+    # Define the terminal package path in the bin directory
+    $terminalPackagePath = Join-Path -Path $binDirectory -ChildPath $terminalPackageOutName
+
+    # Download the terminal package, overwriting if it exists
+    Invoke-WebRequest -Uri $terminalPackageUrl -OutFile $terminalPackagePath -Force
+
+    # Add the app package
+    Add-AppxPackage -Path $terminalPackagePath
+}
+
+# Example call to install Windows Terminal from the default repository
+# Install-WindowsTerminal -workingDirectory "C:\Path\To\WorkingDirectory"
---
+++ src/install-terminal.ps1
@@ -0,0 +1,37 @@
+﻿param (
+    [string]$repo = "microsoft/terminal",
+    [string]$workingDirectory
+)
+
+# Define the module path assuming it's in the same directory as the script
+$modulePath = ".\InstallTerminalModule.psm1"
+
+# Check if the module exists
+if (-not (Test-Path -Path $modulePath)) {
+    Write-Host "The module InstallTerminalModule.psm1 does not exist in the directory: $($PSScriptRoot)"
+    exit 1
+}
+
+# Import the module
+Import-Module -Name $modulePath
+
+# Prompt user for the working directory if not provided
+if (-not $workingDirectory) {
+    $workingDirectory = Read-Host -Prompt "Enter the working directory"
+}
+
+# Install Windows Terminal
+Install-WindowsTerminal -repo $repo -workingDirectory $workingDirectory
+
+<#
+Usage
+You can now run the script with command-line parameters like this:
+
+.\install-terminal.ps1 -repo "microsoft/terminal" -workingDirectory "C:\Path\To\WorkingDirectory"
+Or, if you want to use the default repository and prompt for the working directory:
+
+.\install-terminal.ps1 -workingDirectory "C:\Path\To\WorkingDirectory"
+Or, if you want to prompt for both parameters:
+
+.\install-terminal.ps1
+#>
